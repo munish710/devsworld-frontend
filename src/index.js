f9c9946199acdea25fb9c020a7fdde8c68c036dc
munish710 <munish710@gmail.com>
2021-09-26 00:57:46 +0530

    feat : axios - add baseURL, global reqquest and response interceptors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,34 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import axios from "axios";
+import { logout } from "./app/features/authenticationSlice";
+
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.render(
   <React.StrictMode>
