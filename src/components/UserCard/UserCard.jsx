44c0942897b6a230fa18f6564b72b3d230d89961
munish710 <munish710@gmail.com>
2021-09-27 01:18:57 +0530

    feat : add UserCard component

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Avatar from "../Avatar/Avatar";
+
+const UserCard = ({ user }) => {
+  const { _id: userID, avatarUrl, name, username } = user;
+  return (
+    <Wrapper>
+      <Avatar url={avatarUrl} />
+      <div className="user-info">
+        <Link to={`/profile/${userID}`}>{name}</Link>
+        <h6>@{username}</h6>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.article`
+  background: var(--clr-white);
+  border: 1px solid var(--clr-primary-8);
+  margin: 0.5rem 0;
+  border-radius: var(--radius);
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  gap: 1rem;
+  h6 {
+    font-weight: 400;
+    margin-top: -0.35rem;
+  }
+  /* .user {
+      display: flex;
+      align-items: flex-start;
+      gap: 0.25rem;
+    } */
+  .user-info > a {
+    color: var(--clr-primary-5);
+    font-weight: 500;
+  }
+`;
+
+export default UserCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,5 @@ import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Profile from "./Profile/Profile";
 import PostDetail from "./PostDetail/PostDetail";
+import UserCard from "../components/UserCard/UserCard";
 export { Home, PageNotFound, Login, Signup, Profile, PostDetail };
