26e93934f368791cc77fdcf0ac80a2d8c2e4ca7c
munish710 <munish710@gmail.com>
2021-10-03 23:51:22 +0530

    refactor : UserFeed and GeneralFeed to access global state

--- src/pages/Home/components/GeneralFeed.jsx
+++ src/pages/Home/components/GeneralFeed.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import Loader from "react-loader-spinner";
+
 import { Posts } from "../../../components";
-import { posts } from "../../../utils/mockPosts";
 
 const GeneralFeed = () => {
-  return <Posts posts={posts} />;
+  const { generalFeed, generalFeedStatus } = useSelector((state) => state.feed);
+  return (
+    <Wrapper>
+      {generalFeed.length > 0 ? (
+        <>
+          {" "}
+          <h4>General Feed</h4>
+          <div className="general-feed">
+            {generalFeedStatus === "loading" ? (
+              <Loader
+                type="Oval"
+                color="#6366f1"
+                height="4rem"
+                width="4rem"
+                className="loader"
+              />
+            ) : (
+              <Posts posts={generalFeed} />
+            )}
+          </div>
+        </>
+      ) : null}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 2rem 0;
+  h4 {
+    letter-spacing: normal;
+    color: var(--clr-grey-3);
+    font-weight: 500;
+  }
+  .loader {
+    margin: 4rem;
+    margin-left: 30%;
+  }
+`;
 
 export default GeneralFeed;
--- src/pages/Home/components/UserFeed.jsx
+++ src/pages/Home/components/UserFeed.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Posts } from "../../../components";
+import { getUserFeed } from "../../../app/features/feedSlice";
+import Loader from "react-loader-spinner";
+import styled from "styled-components";
 
 const UserFeed = () => {
-  return <div></div>;
+  const authState = useSelector((state) => state.authentication);
+  const { userFeed, userFeedStatus } = useSelector((state) => state.feed);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    (async function () {
+      if (userFeedStatus === "idle" && authState.token) {
+        await dispatch(getUserFeed());
+      }
+    })();
+  }, [authState.token]);
+  return (
+    <Wrapper>
+      <h4>Your Feed</h4>
+      <div className="user-feed">
+        {userFeedStatus === "loading" ? (
+          <Loader
+            type="Oval"
+            color="#6366f1"
+            height="4rem"
+            width="4rem"
+            className="loader"
+          />
+        ) : (
+          <div>
+            {userFeed.length > 0 ? (
+              <Posts posts={userFeed} />
+            ) : (
+              <p>
+                Your posts and posts from people you follow will appear here!
+                <br />
+                Check posts from other members below
+              </p>
+            )}
+          </div>
+        )}
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 2rem 0;
+  h4 {
+    letter-spacing: normal;
+    color: var(--clr-grey-3);
+    font-weight: 500;
+  }
+  .loader {
+    margin: 4rem;
+    margin-left: 30%;
+  }
+`;
 
 export default UserFeed;
