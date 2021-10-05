65d2681c7749cd98212addf75f35dc8a425e2216
munish710 <munish710@gmail.com>
2021-10-05 14:30:48 +0530

    feat : add postSlice

---
+++ src/app/features/postSlice.js
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//thunks
+
+//create post
+export const createPost = createAsyncThunk(
+  "post/createPost",
+  async (postData) => {
+    try {
+      const response = await axios.post("/posts", postData);
+      if (response.data.success) {
+        return response.data.post;
+      }
+      return {};
+    } catch (error) {
+      console.log("Couldn't create post", error);
+    }
+  }
+);
+//like/unlike post
+export const toggleLike = createAsyncThunk(
+  "post/toggleLike",
+  async (postID) => {
+    try {
+      const response = await axios.patch(`/posts/${postID}/toggle-like`);
+      if (response.data.success) {
+        return response.data.isLiked;
+      }
+    } catch (error) {
+      console.log("Toggle-like failed", error);
+    }
+  }
+);
+
+//add comment
+
+//delete Comment
+
+const initialPostState = {
+  post: {},
+  postStatus: "idle",
+};
+const postSlice = createSlice({
+  name: "post",
+  initialState: initialPostState,
+  reducers: {
+    updatePostInSlice: (state, action) => {
+      state.post = action.payload;
+    },
+  },
+  extraReducers: {
+    [createPost.pending]: (state, action) => {
+      state.postStatus = "loading";
+    },
+    [createPost.fulfilled]: (state, action) => {
+      state.postStatus = "success";
+      state.post = action.payload;
+    },
+    [createPost.rejected]: (state, action) => {
+      state.postStatus = "error";
+    },
+  },
+});
+
+export const { updatePostInSlice } = postSlice.actions;
+export default postSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authenticationReducer from "./features/authenticationSlice";
 import feedReducer from "./features/feedSlice";
+import postReducer from "./features/postSlice";
 
 export default configureStore({
   reducer: {
     authentication: authenticationReducer,
     feed: feedReducer,
+    post: postReducer,
   },
 });
