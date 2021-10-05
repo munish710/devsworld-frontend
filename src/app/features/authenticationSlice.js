358c6cf5deff3ad6866586de4b71e6a9b8a2a467
munish710 <munish710@gmail.com>
2021-10-05 23:47:29 +0530

    refactor : authentication slice

--- src/app/features/authenticationSlice.js
+++ src/app/features/authenticationSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userInfo = {
+  _id: localStorage.getItem("_id") ?? "",
   name: localStorage.getItem("name") ?? "",
   username: localStorage.getItem("username") ?? "",
   token: localStorage.getItem("token") ?? "",
@@ -12,21 +13,25 @@ const authenticationSlice = createSlice({
   initialState: userInfo,
   reducers: {
     login: (state, action) => {
-      const { name, username, token, avatarUrl } = action.payload;
+      const { userID, name, username, token, avatarUrl } = action.payload;
+      localStorage.setItem("_id", userID);
       localStorage.setItem("name", name);
       localStorage.setItem("username", username);
       localStorage.setItem("token", token);
       localStorage.setItem("avatarUrl", avatarUrl);
+      state._id = userID;
       state.name = name;
       state.username = username;
       state.token = token;
       state.avatarUrl = avatarUrl;
     },
     logout: (state, action) => {
+      localStorage.setItem("_id", null);
       localStorage.setItem("name", null);
       localStorage.setItem("username", null);
       localStorage.setItem("token", null);
       localStorage.setItem("avatarUrl", null);
+      state._id = "";
       state.name = "";
       state.username = "";
       state.token = "";
