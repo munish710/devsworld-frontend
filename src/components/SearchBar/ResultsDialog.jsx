dd3d641db69e4859cb63f201d278d18d8aa86de7
munish710 <munish710@gmail.com>
2021-10-10 18:38:47 +0530

    feat : complete search user functionality

---
+++ src/components/SearchBar/ResultsDialog.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import Loader from "react-loader-spinner";
+import { Avatar } from "..";
+import { Link } from "react-router-dom";
+
+const ResultsDialog = ({ showResultsDialog, usersData, isLoading }) => {
+  return (
+    <Dialog className={`${showResultsDialog ? "show-modal" : "hide-modal"}`}>
+      {isLoading ? (
+        <Loader type="Oval" color="#48647f" height="2rem" width="2rem" />
+      ) : (
+        <>
+          {usersData.length > 0 ? (
+            <ul>
+              {usersData.map((user) => {
+                return (
+                  <li key={user._id}>
+                    <article className="result-card">
+                      <Avatar size="small" url={user.avatarUrl} />
+                      <Link to={`profile/${user._id}`}>{user.username}</Link>
+                    </article>
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>No users found</p>
+          )}
+        </>
+      )}
+    </Dialog>
+  );
+};
+
+const Dialog = styled.div`
+  position: absolute;
+  display: grid;
+  place-items: center;
+  padding: 1rem;
+  width: 12rem;
+  top: 3.2rem;
+  background: var(--clr-white);
+  border-radius: var(--radius);
+  border: 1px solid var(--clr-primary-8);
+  height: 14rem;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  ul {
+    height: 100%;
+    width: 100%;
+    .result-card {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      border-bottom: 1px solid var(--clr-primary-8);
+      padding: 0.5rem;
+      margin-bottom: 0.5rem;
+    }
+  }
+`;
+
+export default ResultsDialog;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import ResultsDialog from "./ResultsDialog";
+import { useLocation } from "react-router";
+import axios from "axios";
 
 const SearchBar = () => {
-  return <div>Search</div>;
+  const [showResultsDialog, setShowResultsDialog] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [resultData, setResultData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`/users/search?user=${searchQuery}`);
+        if (response.data.success) {
+          setResultData(response.data.users);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        setIsLoading(false);
+        console.log("Error, while searching for users", error);
+      }
+    })();
+  }, [searchQuery]);
+
+  useEffect(() => {
+    setShowResultsDialog(false);
+    setSearchQuery("");
+  }, [pathname]);
+  return (
+    <SearchInput>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Search User"
+          onClick={() => setShowResultsDialog(true)}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="input-icon">
+          {showResultsDialog ? (
+            <AiOutlineClose onClick={() => setShowResultsDialog(false)} />
+          ) : (
+            <BsSearch />
+          )}
+        </div>
+      </div>
+      <ResultsDialog
+        showResultsDialog={showResultsDialog}
+        usersData={resultData}
+        isLoading={isLoading}
+      />
+    </SearchInput>
+  );
 };
+
+const SearchInput = styled.div`
+  background: var(--clr-white);
+  .input-container {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+    border-radius: var(--radius);
+    border: 1px solid var(--clr-primary-8);
+
+    input {
+      outline: none;
+      border: none;
+      padding: 0.5rem;
+      width: 85%;
+    }
+    .input-icon {
+      cursor: pointer;
+      color: var(--clr-grey-4);
+      top: 0.5rem;
+      left: 80%;
+      &:hover {
+        color: var(--clr-grey-2);
+      }
+    }
+  }
+`;
 
 export default SearchBar;
