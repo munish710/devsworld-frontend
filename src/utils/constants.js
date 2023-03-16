47b6bdccdf720a3e1a73f8a85eaefddae7ad7fb4
Munish Thakur <53472402+munish710@users.noreply.github.com>
2023-03-17 00:08:12 +0530

    chore : change backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const CLOUDINARY_URL = process.env.REACT_APP_CLOUDINARY_URL;
 export const CLOUDINARY_PRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
 export const BASE_API_URL =
-  "https://devs-world-backend-api.herokuapp.com/api/v1";
+  "https://devsworld-backend.onrender.com/api/v1";
